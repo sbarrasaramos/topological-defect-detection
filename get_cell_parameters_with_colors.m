cebf2b6ac4256c2c8916d1aa4a434d9a57a75fce
Sara Barrasa Ramos <sbarrasaramos@gmail.com>
2022-05-23 13:49:08 +0200

    small cells off to function

--- get_cell_parameters_with_colors.m
+++ get_cell_parameters_with_colors.m
@@ -44,21 +44,7 @@ I = image2binary(image2binary_flag, origin_filename, analysis_foldername, cell_c
 I = borderoff(borderoff_flag, I, analysis_foldername);
 
 %% erase small components and visualize binary image before and after filtering by size
-if smalloff_flag > 0
-    cell_sizes = regionprops(I, 'Area');
-    max_cell_size = max([cell_sizes.Area]);
-    min_cell_size = mean([cell_sizes.Area])/10; 
-    I = bwareafilt(I,[min_cell_size max_cell_size],4);
-    if smalloff_flag > 1
-        figure, imshow(I);
-        if smalloff_flag > 2
-            saveas(gcf,fullfile(analysis_foldername, sprintf('00%d-binary_WOSmall.tif',j)));
-        end
-    end
-    clear cell_sizes  max_cell_size min_cell_size
-else
-    warning("Very small cells are usually the result of issues during the segmentation process.")
-end
+I = smalloff(smalloff_flag, I, analysis_foldername);
 
 %% find label and visualize all connected components = cells
 if cc_flag > 0
